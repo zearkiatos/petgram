56e22ad2ad862c5f06fa263f7c3510bc6f95505b
Pedro Capriles <caprilespe@outlook.com>
2020-07-04 18:44:47 -0400

    SVGR SVG to component of ReactJS

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { CategoriesList } from './components/CategoriesList'
 import { GlobalStyles } from './shared/styles/GlobalStyles'
 import { PhotoCardsList } from './components/PhotoCardsList'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyles />
+    <Logo />
     <CategoriesList />
     <PhotoCardsList />
   </div>
