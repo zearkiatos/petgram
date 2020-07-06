e23c361540962478c2ca0bc3090096f000509709
Pedro Capriles <caprilespe@outlook.com>
2020-07-06 00:31:45 -0400

    Using hooks

--- src/components/CategoriesList/index.js
+++ src/components/CategoriesList/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const CategoriesList = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    window.fetch('https://petgram-server.zearkiatos.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
   return (
     <List>
       {
