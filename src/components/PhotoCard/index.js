8dbc25bb054837a32a6c71c08a4a5a3c17fcf8f3
Pedro Capriles <caprilespe@outlook.com>
2020-07-16 16:14:57 -0400

    Use the localStorage for save the likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { ImageWrapper, Image, Button, Article } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
-import { keyframes } from 'styled-components'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
