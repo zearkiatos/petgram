39e49bfec02e7f9f9c03f7d78cbe46ee16d8febc
Pedro Capriles <caprilespe@outlook.com>
2020-07-16 16:14:22 -0400

    Use the localStorage for save the likes

--- src/components/PhotoCardsList/index.js
+++ src/components/PhotoCardsList/index.js
@@ -4,7 +4,7 @@ import { PhotoCard } from '../PhotoCard'
 export const PhotoCardsList = () => {
   return (
     <ul>
-      {[1, 2, 3, 4, 5].map(photoCard => <li key={photoCard}><PhotoCard /></li>)}
+      {[1, 2, 3, 4, 5].map(photoCardId => <li key={photoCardId}><PhotoCard id={photoCardId} /></li>)}
     </ul>
   )
 }
