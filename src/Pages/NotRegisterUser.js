4c4df1cc31a21d1e2563aa635346f3475d46010f
Pedro Capriles <caprilespe@outlook.com>
2020-08-11 23:46:52 -0400

    Mutation for login user

--- src/Pages/NotRegisterUser.js
+++ src/Pages/NotRegisterUser.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../containers/RegisterMutation'
+import { LoginMutation } from '../containers/LoginMutation'
 
 export const NotRegisterUser = () => (
   <Context.Consumer>
@@ -16,13 +17,24 @@ export const NotRegisterUser = () => (
                   const variables = { input }
                   register({ variables }).then(activateAuth)
                 }
-                const errorMessage = error && 'The user already exist or something was wrong.'
+                const errorMessage = error && '⛔ The user already exist or something was wrong.'
                 return <UserForm onSubmit={onSubmit} title='Register' error={errorMessage} disabled={loading} />
               }
             }
           </RegisterMutation>
-
-          <UserForm onSubmit={activateAuth} title='Log In' />
+          <LoginMutation>
+            {
+              (login, { data, loading, error }) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  login({ variables }).then(activateAuth)
+                }
+                const errorMessage = error && '⛔ The user not exist or Something was wrong.'
+                return <UserForm onSubmit={onSubmit} title='Log In' error={errorMessage} disabled={loading} />
+              }
+            }
+          </LoginMutation>
         </>
       }
     }
