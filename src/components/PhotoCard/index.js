5bce2ceecdde3dbfea2f235873694e6b233aeda1
Pedro Capriles <caprilespe@outlook.com>
2020-08-17 15:33:37 -0400

    Do like as user register

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { ImageWrapper, Image, Article } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation'
 import { Link } from '@reach/router'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, ref] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
   return (
     <Article ref={ref}>
       {show && (
@@ -24,12 +21,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             {
               (toggleLike) => {
                 const handleFavClick = () => {
-                  !liked && toggleLike({
+                  toggleLike({
                     variables: {
                       input: { id }
                     }
                   })
-                  setLiked(!liked)
                 }
                 return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
               }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,23 @@ import Context from './Context'
 const { BASE_API_URL } = process.env
 
 const client = new ApolloClient({
-  uri: `${BASE_API_URL}/graphql`
+  uri: `${BASE_API_URL}/graphql`,
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: error => {
+    const { networkError } = error
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 
 ReactDOM.render(
